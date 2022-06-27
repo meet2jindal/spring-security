6d2e7e7cc73ad92d85dc39d3dc94fef8b3cff3e7
Naresh <meet2jindal@gmail.com>
2022-06-27 12:39:42 +0530

    initial commit

---
+++ spring-security-basic/src/main/java/springsecurity/springsecuritybasic/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package springsecurity.springsecuritybasic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests().anyRequest().authenticated()
+                .and()
+                .formLogin(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/test");
+    }
+
+}
